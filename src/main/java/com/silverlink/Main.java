1813e88e3be12ee89043794ac01e179ec21c4426
Ninorokr <ninorokr2@gmail.com>
2023-02-19 06:10:50 -0500

    ExcelGatherer oracion y parrafo actualizados

--- src/main/java/com/silverlink/Main.java
+++ src/main/java/com/silverlink/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         NTSCE ntsce = new NTSCE();
         ntsce.generar();
+
+//        ExcelGatherer eg = new ExcelGatherer();
+//        eg.extract("D:\\Apache POI test\\BVERITAS_AV02-23.xlsx");
     }
 }
